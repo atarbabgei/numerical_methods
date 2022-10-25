98caea925b061af7d8537285cb4a56b08df7dbe5
Atar Babgei <atarbabgei@gmail.com>
2022-10-25 11:30:04 +0700

    First commit on interpolation and curve fitting

---
+++ 03_interpolation_and_regression/03_linear_regression_for_loop.py
@@ -0,0 +1,20 @@
+'''
+Method: Linear Regression (for-loop)
+Section: Interpolation and Curve Fitting
+''' 
+from numpy import array, sum, mean
+x = array([3, 4, 5, 6, 7, 8],float)
+y = array([0, 7, 17, 26, 35, 45],float)
+n = len(x)
+sumx = sumx2 = sumxy = sumy = 0
+for i in range(n):
+    sumx += x[i]
+    sumx2 += x[i]**2
+    sumxy += x[i]*y[i]
+    sumy += y[i]
+xm = sumx / n
+ym = sumy / n
+a = (ym*sumx2 - xm*sumxy)/(sumx2 - n*xm**2)
+b = (sumxy - xm*sumy)/(sumx2 - n*xm**2)
+print('The straight line equation')
+print('y = (%.3f) + (%.3f)x' % (a,b))
---
+++ 03_interpolation_and_regression/04_linear_regression_numpy.py
@@ -0,0 +1,12 @@
+'''
+Method: Linear Regression (using NumPy functions)
+Section: Interpolation and Curve Fitting
+''' 
+from numpy import array, sum, mean
+x = array([3, 4, 5, 6, 7, 8])
+y = array([0, 7, 17, 26, 35, 45])
+n = len(x);      
+a = (mean(y)*sum(x**2)-mean(x)*sum(x*y))/(sum(x**2)-n*mean(x)**2);
+b = (sum(x*y)-mean(x)*sum(y))/(sum(x**2)-n*mean(x)**2)
+print('The straight line equation:')
+print('f(x) = (%.3f) + (%.3f)x'%(a,b))
